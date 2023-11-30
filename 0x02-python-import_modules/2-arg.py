2ff114fbf4b3ce1afb1a168b8df1454a37211a1a
Kelvin <koolkt10@gmail.com>
2023-11-30 11:46:03 -0800

    calculator and args

---
+++ 0x02-python-import_modules/2-arg.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+if __name__ = "__main__"
+import sys
+
+count = 0
+if len(sys.argv) == 1:
+    print("{} arguments.".format(count))
+elif len(sys.argv) == 2:
+    count = 1
+    print("{} argument:".format(count))
+else:
+    count = len(sys.argv) - 1
+    print("{} arguments:".format(count))
+    j = 1
+    while j < count + 1:
+        print("{}: {}".format(j, sys.argv[j]))
+        j += 1
