2f7d8932230f0e50a48031210eed9d66268e2bdb
Kelvin <koolkt10@gmail.com>
2023-12-04 14:48:01 -0800

    is_palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -6,7 +6,32 @@
  * Return: 1 on success, 0 otherwise
  */
 
-int is_palindrome(listint_t **head);
+int is_palindrome(listint_t **head)
 {
+	int list[1000];
+	int i = 0, j, k;
+	listint_t *temp;
 
+	if (*head == NULL)
+		return (1);
+
+	temp = *head;
+	while (temp != NULL)
+	{
+		list[i] = temp->n;
+		i++;
+		temp = temp->next;
+	}
+
+	j = (i + 1) / 2;
+
+	for (k = 0; k < j; k++)
+	{
+		if (list[k] == list[i - k - 1])
+		{
+		}
+		else
+			return (0);
+	}
+	return (1);
 }
