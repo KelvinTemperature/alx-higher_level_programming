1933a65360b444c8523e2ef115f201b44496fe46
Kelvin <koolkt10@gmail.com>
2023-12-08 11:27:02 -0800

    roman numerals corrected

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,10 @@ def roman_to_int(roman_string):
     romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num_int = 0
 
-    if not isinstance(roman_string, str) or roman_string is None:
+    if not roman_string:
+        return 0
+
+    if not isinstance(roman_string, str):
         return 0
 
     if len(roman_string) == 0:
