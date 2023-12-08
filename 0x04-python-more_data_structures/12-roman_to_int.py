ec51b10aaf68126ce013882d56c677ca3a56aa1d
Kelvin <koolkt10@gmail.com>
2023-12-08 14:09:13 -0800

    tired

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,10 +3,7 @@ def roman_to_int(roman_string):
     romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num_int = 0
 
-    if not roman_string:
-        return 0
-
-    if not isinstance(roman_string, str):
+    if not roman_string or not isinstance(roman_string, str):
         return 0
 
     if len(roman_string) == 0:
