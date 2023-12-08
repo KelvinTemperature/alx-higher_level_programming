a90505ac4453845c21ffd6a8e2a02de4d19e9ce6
Kelvin <koolkt10@gmail.com>
2023-12-08 11:13:46 -0800

    roman numerals implementation

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num_int = 0
 
-    if not roman_string or roman_string is None:
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
 
     if len(roman_string) == 0:
@@ -17,6 +17,15 @@ def roman_to_int(roman_string):
             num_int = romans[roman_string[1]] - romans[roman_string[0]]
         i = 2
         while i < len(roman_string):
-            num_int += romans[roman_string[i]]
-            i += 1
+            if i == len(roman_string) - 1:
+                if romans[roman_string[i]] > romans[roman_string[i - 1]]:
+                    num_int -= romans[roman_string[i - 1]]
+                    num_int += (romans[roman_string[i]] - romans[roman_string[i - 1]])
+                    i += 1
+                else:
+                    num_int += romans[roman_string[i]]
+                    i += 1
+            else:
+                num_int += romans[roman_string[i]]
+                i += 1
         return num_int
