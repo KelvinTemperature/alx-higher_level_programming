ed1b0434c8d09543f5eb4cea1d1f54428cb173b9
Kelvin <koolkt10@gmail.com>
2023-12-18 17:42:30 -0800

    safe print all

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    j = 0
+    while i < x:
+        try:
+            print(my_list[i], end='')
+        except:
+            break
+        else:
+            j += 1
+        i += 1
+    print()
+
+    return j
