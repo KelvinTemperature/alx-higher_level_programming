f5a7db0d4610c356d21a5c0a14be07906367b079
Kelvin <koolkt10@gmail.com>
2023-12-19 17:38:27 -0800

    square 4-6

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Square Module"""
+
+
+class Square:
+    """Class that defines a square."""
+
+    def __init__(self, size=0):
+        """Method to initialize the square object."""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Property Setter for the size param
+
+        Returns:
+                size of the square
+        """
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Property Setter for the size param
+        Args:
+            Param1 (int): value for size
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = int(value)
+
+    def area(self):
+        """Method Area defines the area of the square object
+
+        Returns:
+                square of the size of the square
+
+        """
+
+        return self.__size ** 2
---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+"""Square Module"""
+
+
+class Square:
+    """Class that defines a square."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Method to initialize the square object."""
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        """Property Getter for the size param
+
+        Returns:
+                size of the square
+        """
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Property Setter for the size param
+        Args:
+            Param1 (int): value for size
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = int(value)
+
+    @property
+    def position(self):
+        """Property Getter for the size param
+
+        Returns:
+                position of the square
+        """
+
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Property Setter for the position param
+        Args:
+            Param1 (tuple): value of position
+        """
+        if not isinstance(value, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def area(self):
+        """Method Area defines the area of the square object
+
+        Returns:
+                square of the size of the square
+
+        """
+
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints a square of size to the stdout
+        """
+
+        if self.__size == 0:
+            print()
+        else:
+            i = 0
+            while (i < self.__size):
+                print(" " * self.__position[0], end='')
+                j = 0
+                for j in range(self.__size):
+                    print("#", end='')
+                print()
+                i += 1
