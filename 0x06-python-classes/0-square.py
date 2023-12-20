4bad1c1dac41cd526d4fcfaf7593d6af2d049291
Kelvin <koolkt10@gmail.com>
2023-12-19 16:43:08 -0800

    square 0-3

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Square Module"""
+
+
 class Square:
-    '''Class Square defines a square.'''
+    """Class Square defines a square."""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Square Module"""
+
+
 class Square:
-    '''Class Square defines a square object'''
+    """Class Square defines a square object"""
 
     def __init__(self, size):
         """ Initialize method that stores the size of the square
