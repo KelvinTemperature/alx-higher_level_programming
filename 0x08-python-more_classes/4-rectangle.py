558622e1a8c18021f2a48bf6f25dc4192dbcbb5c
Kelvin <koolkt10@gmail.com>
2024-01-04 17:45:58 -0800

    rectangle problems

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""Rectangle Module"""
+
+
+class Rectangle():
+    """class Rectangle defines a rectangle"""
+    def __str__(self):
+        rtn = ""
+        i = 0
+
+        if self.__width == 0 or self.__height == 0:
+            return rtn
+
+        while i < self.__height:
+            for j in range(self.__width):
+                rtn += '#'
+            if i != self.__height - 1:
+                rtn += '\n'
+            i += 1
+        return rtn
+
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
