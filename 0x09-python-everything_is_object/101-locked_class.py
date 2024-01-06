ec31ecc8624858377d32afbf46b74ea215c926bc
Kelvin <koolkt10@gmail.com>
2024-01-05 18:24:53 -0800

    locked class worked

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""
-
-This is a module that containts a class that avoids
+"""module that containts a class that avoids
 dynmaically created attributes
-
 """
 
 
 class LockedClass:
+    """Locked Class"""
+
     __slots__ = ['first_name']
 
     def __init__(self):
