8fea227d4996e19059e90adf8585de1b5c3413a1
Kelvin <koolkt10@gmail.com>
2024-01-10 16:37:48 -0800

    advanced tasks

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Numpy Module to Multiply Matrix"""
+
+
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """Function to multiply two matrix using numpy"""
+
+    return np.matmul(m_a, m_b)
