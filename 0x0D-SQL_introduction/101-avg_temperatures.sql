e61c7f5acfc2e9c1590110670c3bf9600cb11652
Kelvin <koolkt10@gmail.com>
2024-02-13 20:17:57 -0800

    advanced tasks

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,6 @@
+--displays average temperature
+--displays average temperature by city
+SELECT city, AVG(value) AS avg_temp 
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,6 @@
+--displays average temperature
+--displays average temperature by city
+SELECT state, MAX(value) AS max_temp 
+FROM temperatures
+GROUP BY state
+LIMIT 3;
