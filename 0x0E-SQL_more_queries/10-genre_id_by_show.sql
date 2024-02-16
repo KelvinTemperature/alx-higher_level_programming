932b1c0af56cc21c1fb189f90db761e5d63f0df6
Kelvin <koolkt10@gmail.com>
2024-02-15 20:20:03 -0800

    some more tasks

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- list all show in the database
+-- Lists all the tvshows in the DB
+SELECT title, genre_id
+FROM tv_shows AS s INNER JOIN tv_show_genres AS g
+ON s.id=g.show_id
+ORDER BY s.title, g.genre_id;
