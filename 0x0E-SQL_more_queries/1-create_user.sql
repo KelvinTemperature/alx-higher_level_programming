8759b744b41554cb719591f70f4c12547d96950d
Kelvin <koolkt10@gmail.com>
2024-02-16 15:03:44 -0800

    more correction

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Creates a user on the server
 -- CReates User user_0d_1 on the server
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'USER_0d_1_pwd';
-GRANT ALL ON *.* TO user_0d_1@localhost;
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,7 @@
 -- list all show in the database
 -- Lists all the tvshows in the DB
 SELECT title, genre_id
-FROM tv_shows AS s FULL OUTER JOIN tv_show_genres AS g
+FROM tv_shows AS s LEFT JOIN tv_show_genres AS g
 ON s.id=g.show_id
-ORDER BY s.title, g.genre_id;
+WHERE g.genre_id IS NULL
+ORDER BY s.title;
