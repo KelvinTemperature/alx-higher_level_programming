435bc45cc7e61f0885cc4e79c340f1e001f87f6d
Kelvin <koolkt10@gmail.com>
2024-02-15 16:15:15 -0800

    some tasks

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Creates a user on the server
+-- CReates User user_0d_1 on the server
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'USER_0d_1_pwd';
---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,3 @@
+-- Script to create Table
+-- Query creates Table force_name
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256));
