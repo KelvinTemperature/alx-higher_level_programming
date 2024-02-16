287ad5b3f968f5c78e416dda91fa9c2427b6680e
Kelvin <koolkt10@gmail.com>
2024-02-16 14:41:11 -0800

    corrections

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- Script to grant priviledges to users
 -- Query to grant all priviledges to user_0d_1 and user_0d_2
-GRANT ALL ON *.* TO user_0d_1@localhost;
-GRANT ALL ON *.* TO user_0d_2@localhost;
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
