860ff7fd44cb722f0a1dc7112f25aa6a753230e5
Kelvin <koolkt10@gmail.com>
2024-03-28 22:05:54 -0700

    fixes

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Displays all HTTP methods the servver will accept
-curl -sI -X OPTIONS "$1" | grep "ALLOW:" | cut -d " " -f 2-
+curl -sI -X OPTIONS "$1" | grep -i "^ALLOW:" | cut -d " " -f 2-
