55fbcf4c8aaf206e97bce4a20f7a6697dd6b4464
Kelvin <koolkt10@gmail.com>
2024-03-28 19:55:36 -0700

    urllib tasks

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 # Script to display the X-Request-Id of a URL
-import urllib.request
+from urllib import request
 import sys
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen(sys.argv[1]) as response:
+    with request.urlopen(sys.argv[1]) as response:
         html = response.info()
         print(html.get('X-Request-Id'))
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 # Script that sends a post request
-import urllib.request
-import urllib.parse
+from urllib import request, parse
 import sys
 
 
 if __name__ == "__main__":
     value = {'email': sys.argv[2]}
-    data = urllib.parse.urlencode(value)
+    data = parse.urlencode(value)
     data = data.encode('ascii')
-    req = urllib.request.Request(data)
+    req = request.Request(data)
 
-    with urllib.request.urlopen(req) as response:
+    with request.urlopen(req) as response:
         html = response.read()
         print(html.decode('utf-8'))
